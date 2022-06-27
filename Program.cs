690d6fc97dae5bb503c3630bb67a3aa7c8f4e0ec
Gunter <kaiz@yandex.ru>
2022-06-27 13:54:31 +0300

    seminar1 end

--- Program.cs
+++ Program.cs
@@ -1,33 +1,86 @@
 ﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 int n1, n2;
+int min;
+int max;
 
-n1 = 5;
-n2 = 8;
-
-Console.Write("output max and min number ");
+Console.WriteLine("enter first number");
 n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter second number");
 n2 = Convert.ToInt32(Console.ReadLine());
 
-if(n1>n2)
- {
-    Console.WriteLine("number" + n1 + "is greater than" + n2); 
- }
+if (n1<n2)
+{
+   min = n1;
+   max = n2;
+}
 else
 {
-    Console.WriteLine("number" + n2 + "is greater than" + n1);
+   min = n2;
+   max = n1;
 }
-   
+
+Console.WriteLine("min number" + min);
+Console.WriteLine("max number" + max);
+*/
 
 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+ /*
+ int n1, n2, n3;
+ 
+Console.WriteLine("enter first number");
+  n1 = Convert.ToInt32(Console.ReadLine());
+ 
+ Console.WriteLine("enter second number");
+  n2 = Convert.ToInt32(Console.ReadLine());
+ 
+ Console.WriteLine("enter third number");
+  n3 = Convert.ToInt32(Console.ReadLine());
+ 
+ int max = n1;
+ 
+ if (n2 > max) max = n2;
+ if (n3 > max) max = n3;
+ 
+ Console.WriteLine("max = " + max);
+ */
 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+int n1;
 
+Console.WriteLine("enter a number");
+ n1 = Convert.ToInt32(Console.ReadLine());
+ if (n1 % 2 == 0)
+ {
+     Console.WriteLine(n1 + " - an even number");
+ }
+ else
+ {
+     Console.WriteLine(n1 + " - odd number");
+ }
+*/
 
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+ int n1, n2;
 
+ Console.WriteLine("Enter number ");
+ n1 = Convert.ToInt32(Console.ReadLine());
+ n2 = 1;
+ while (n2 < n1)
+ {
+ if (n2 % 2 == 0)
+ {
+     Console.Write(n2 + " ");
+ }
+ n2 = n2 + 1;
+ }
+*/
 
-
-
-
+// 
 
 
 
